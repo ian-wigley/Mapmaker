76722766b3df2e3f4b32127bfae30c243f0ae2c3
Ian Wigley <ian.wigley@outlook.com>
2022-12-30 09:23:16 +0000

    Update Screen.cs
    Update the Screen.cs.

--- mapmaker/mapmaker/Screen.cs
+++ mapmaker/mapmaker/Screen.cs
@@ -24,12 +24,48 @@ namespace mapmaker
         public string GetDataAsString(int count)
         {
             StringBuilder sb = new StringBuilder();
-            for (int i = count; i < 14 + count; i++)
+            for (int j = count; j < 14 + count; j++)
             {
-                sb.Append(Tiles[i].TileNumber + ",");
+                sb.Append(Tiles[j].TileNumber + ",");
             }
             return sb.ToString();
         }
+
+        public string GetDataAsJsonString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("{ \"rectangleList\": [],");
+            sb.Append("\"tiles\": [ {");
+
+            //"tiles": [
+            //{
+            //    "_comment": "Upper Rocks",
+            //    "sx": 0,
+            //    "sy": 0,
+            //    "sw": 100,
+            //    "sh": 117,
+            //    "dx": 0,
+            //    "dy": 170,
+            //    "dw": 100,
+            //    "dh": 117
+            //},
+
+            for (int i = 0; i < Tiles.Count; i += 14)
+            {
+                for (int j = i; j < 14 + i; j++)
+                {
+                    // Ignore empty tiles
+                    if (Tiles[j].TileNumber != 4)
+                    {
+                        sb.Append(Tiles[j].TileNumber + ",");
+                    }
+                }
+            }
+            sb.Append("] }");
+            return sb.ToString();
+        }
+
+
 
         [JsonIgnore]
         public List<Tile> Tiles { get; private set; } = new List<Tile>();
